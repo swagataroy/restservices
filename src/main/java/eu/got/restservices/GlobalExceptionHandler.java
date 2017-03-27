2ce972deb2071ac6a762f8210d0a5ae6ce99fc54
Swagata Roy <sroy@pivotal.io>
2017-03-27 16:11:06 -0400

    Update GlobalExceptionHandler.java

--- src/main/java/eu/got/restservices/GlobalExceptionHandler.java
+++ src/main/java/eu/got/restservices/GlobalExceptionHandler.java
@@ -25,26 +25,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 
-	@ExceptionHandler(FileNotFoundException.class)
-	public ResponseEntity<?> handle(FileNotFoundException
-			rnfe, HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.APPLICATION_JSON);
-	    //If you don't flush the response it goes into a loop
-		response.flushBuffer();
-		
-		logger.info("Inside GlobalExceptionHandler FileNotFoundException" );
+	@ExceptionHandler(FileNotFoundException.class)
+	public ResponseEntity<ErrorDetail> handle(FileNotFoundException rnfe) {
 		ErrorDetail errorDetail = new ErrorDetail();
 		errorDetail.setTimeStamp(new Date().getTime());
 		errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
 		errorDetail.setTitle("Resource Not Found");
 		errorDetail.setDetail(rnfe.getMessage());
 		errorDetail.setDeveloperMessage(rnfe.getClass().getName());
-
-		return new ResponseEntity<>(errorDetail, headers, HttpStatus.NOT_FOUND);
-		//return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetail);
 	}
+	
 	
 	
 
