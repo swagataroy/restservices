ab42d6977ae2558da7ab7fe67c07fd879813acee
swagatar <sroy@pivotal.io>
2017-03-27 17:37:42 -0400

    Adding new handler

--- src/main/java/eu/got/restservices/GlobalExceptionHandler.java
+++ src/main/java/eu/got/restservices/GlobalExceptionHandler.java
@@ -38,8 +38,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 	
 	
-	
+	/*
+	@ExceptionHandler(FileNotFoundException.class)
+	public ResponseEntity<?> handle(FileNotFoundException
+			rnfe, HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+	    //If you don't flush the response it goes into a loop
+		response.flushBuffer();
+		
+		logger.info("Inside GlobalExceptionHandler FileNotFoundException" );
+		ErrorDetail errorDetail = new ErrorDetail();
+		errorDetail.setTimeStamp(new Date().getTime());
+		errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+		errorDetail.setTitle("Resource Not Found");
+		errorDetail.setDetail(rnfe.getMessage());
+		errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+
+		return new ResponseEntity<>(errorDetail, headers, HttpStatus.NOT_FOUND);
+		//return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+	}
+	
+*/
 
 	@ExceptionHandler(value = IOException.class)
 	public ResponseEntity<?> handle(IOException ex, HttpServletResponse response) throws IOException {
@@ -56,24 +77,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
 	}
 	
-    @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(final HttpMediaTypeNotSupportedException ex, final HttpHeaders headers,
-            final HttpStatus status, final WebRequest request) {
-        logger.info(ex.getClass().getName());
-        //
-        final ErrorDetail errorDetail = new ErrorDetail();
-        final StringBuilder builder = new StringBuilder();
-        builder.append(ex.getContentType());
-        builder.append(" media type is not supported. Supported media types are ");
-        // ex.getSupportedMediaTypes().forEach(t -> builder.append(t + " "));
-
-        errorDetail.setTimeStamp(new Date().getTime());
-        errorDetail.setStatus(status.value());
-        errorDetail.setTitle("Method not supported");
-        errorDetail.setDetail(builder.toString());
-        errorDetail.setDeveloperMessage(ex.getClass().getName());
-
-        return handleExceptionInternal(ex, errorDetail, headers, status, request);
-    }
+   
 
 }
